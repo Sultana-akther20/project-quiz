764ea5204189747a11658c6c32c727d94bbbf496
Sultana Akther <sultana.akther2805@gmail.com>
2025-01-17 13:01:46 +0000

    Save the js file for questions and save all the files

---
+++ assets/css/js/script.js
@@ -0,0 +1,55 @@
+let quizs=[{
+    question: "What is the capital of city of England?",
+    answers: [
+        { text: "London", correct: true},
+        { text: "Barmingham", correct: false},
+        { text: "Luton", correct: false},
+        { text: "Manchester", correct: false},
+    ]
+},
+{
+    question: "What currency is called in the UK?",
+    answers: [
+        { text: "Euro", correct: false},
+        { text: "Pounds Sterling", correct: true},
+        { text: "Dollar", correct: false},
+        { text: "Taka", correct: false},
+    ]   
+},
+{
+    question: "Which one is UK's timezone?",
+    answers: [
+        { text: "GMT", correct: true},
+        { text: "EST", correct: false},
+        { text: "CET", correct: false},
+        { text: "PST", correct: false},
+    ]     
+},
+{
+    question: "What does BBC stand for??",
+    answers: [
+        { text: "British Bigest Cat ", correct: false},
+        { text: "British Baking Contest", correct: false},
+        { text: "Best Broadcasting Channel", correct: false},
+        { text: "British Broadcasting Corporation", correct: true},
+    ]     
+},
+{
+    question: "When is Halloween?",
+    answers: [
+        { text: "28 FEB", correct: false},
+        { text: "14 FEB", correct: false},
+        { text: "31 OCT", correct: true},
+        { text: "25 DEC", correct: false},
+    ]     
+},
+{
+    question: "What is the largest country in the UK?",
+    answers: [
+        { text: "Northern Ireland", correct: false},
+        { text: "England", correct: true},
+        { text: "Wales", correct: false},
+        { text: "Scotland", correct: false},
+    ]        
+}
+]
