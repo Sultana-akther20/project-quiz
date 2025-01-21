7c72782935954d707a84b7d8ce81726b4391090e
Sultana Akther <sultana.akther2805@gmail.com>
2025-01-21 11:52:02 +0000

    Add the quizs for array

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,4 +1,4 @@
-let quizs=[{
+const quizs=[{
     question: "What is the capital of city of England?",
     answers: [
         { text: "London", correct: true},
@@ -52,4 +52,88 @@ let quizs=[{
         { text: "Scotland", correct: false},
     ]        
 }
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerElement = document.getElementById("div");
+const nextElement = document.getElementsByClassName("next")[0];
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextElement.innerHTML="Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = quizs[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerHTML=answer.text;
+        button.classList.add("btn");
+        
+        answerElement.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct=answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextElement.style.display = "none";
+    while(answerElement.firstChild){
+        answerElement.removeChild(answerElement.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerElement.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+
+        }
+        button.disabled = true;
+    });
+    nextElement.style.display = "block";
+    }
+
+    function showScore(){
+        resetState();
+        questionElement.innerHTML = `your score ${score} out of ${quizs.length}!`;
+        nextElement.innerHTML="Play Again";
+        nextElement.style.display ="block";
+    }
+    function handleNextButton(){
+        currentQuestionIndex++;
+        if(currentQuestionIndex < quizs.length){
+            showQuestion();
+        }else{
+            showScore();
+        }
+        }
+    
+    nextElement.addEventListener("click", ()=>{
+        if(currentQuestionIndex < quizs.length){
+            handleNextButton();
+        }else{
+startQuiz();
+        }
+        
+    });
+startQuiz();
