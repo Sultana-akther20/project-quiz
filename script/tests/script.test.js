b0c1171dda856640c8467f7c8196ace9cdcc305e
Sultana-akther20 <sultana.akther2805@gmail.com>
2025-01-25 02:29:17 +0000

    Save the first test for username

--- script/tests/script.test.js
+++ script/tests/script.test.js
@@ -1,1 +1,19 @@
-const  = require("../script");
+const { describe } = require("yargs");
+const {formSubmit}= require("../script");
+const { test } = require("picomatch");
+const expectExport = require("expect");
+
+beforeAll(() => {
+    let fs =require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("form contain correct keys", () => {
+    test("UserName key exists", () => {
+    expectExport("UserName", in form).toBe(true);
+
+    });
+});
