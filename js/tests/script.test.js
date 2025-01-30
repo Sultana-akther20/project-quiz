de6a3af0967cd0724b48ec0dfa718779cc0b65ad
Sultana Akther <sultana.akther2805@gmail.com>
2025-01-30 21:47:25 +0000

    save all the pages

--- js/tests/script.test.js
+++ js/tests/script.test.js
@@ -1,14 +1,10 @@
-const $ = require("jquery");
-const {showScore}= require("../script");
+
+const {showScore}= require("../script.js");
 
 
-
-
-
- document.body.innerHTML=`<div id="question"></div> <div id="div"></div> <button id="next"></button>`;
         
     describe("showScore function", () => {
-        test("should show the score for after finishing the quiz", () =>{
+        test("should show the score after finishing the quiz", () =>{
             const $questionElement = $ ("#question");
             const $nextElement = $ ("#next");
             const quizLength = 10;
